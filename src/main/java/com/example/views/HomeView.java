8e4741088798ca17331eefb0cd27e1517e185cf2
Matt <mjcantin@yahoo.com>
2025-11-17 15:22:47 -0700

    Set up vaadin so that we can deploy Java webapp SPA (#5)
    * Set up vaadin to create java SPA webapp
    
    ---------
    
    Co-authored-by: Matt Cantin <mcantin@msudenver.edu>

---
+++ src/main/java/com/example/views/HomeView.java
@@ -0,0 +1,8 @@
+package com.example.views;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+
+@Route("")   // Landing page
+public class HomeView extends VerticalLayout {
+}
